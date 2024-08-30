76a3d2badc777748863193f6face5f443498dc3b
unknown <gdutt2304@gmail.com>
2024-08-30 18:34:08 +0530

    Class 1  (intro)

---
+++ Class1.java
@@ -0,0 +1,29 @@
+package CoreJava;
+
+public class Class1 {
+    public static void main(String[] args) {
+      String name =   "Ganesh Dutt";
+
+        int number  = 20;
+//     byte number =  3;
+        byte  a = 1;
+        short  b = 1;
+         int  c = 1;
+         long d = 1;
+
+//         float number1 = 45.323f;
+         float number1 = 45.323432f;
+         double number2 = 45.3234328784829829389289389347487837487;
+        System.out.println(number2);
+//        System.out.println(number1);
+
+
+
+
+        System.out.println(a);
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println("Hello World");
+
+    }
+}
