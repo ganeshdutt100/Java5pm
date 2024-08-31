3d81ef5b42778cc1d5d9f4dd227d20042727c8ec
unknown <gdutt2304@gmail.com>
2024-08-31 17:45:26 +0530

    corejava

---
+++ Class2.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class Class2 {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 40;
+         int sum  = a +b ;
+         int c  = a -b ;
+        System.out.println(  sum);
+        System.out.print(c);
+
+    }
+}
