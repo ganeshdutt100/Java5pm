2c4165a5600d740b71f291cf8974d77cf666b19c
unknown <gdutt2304@gmail.com>
2024-08-31 17:58:22 +0530

    Relational Operators:

--- Class2.java
+++ Class2.java
@@ -4,12 +4,22 @@ public class Class2 {
     public static void main(String[] args) {
 
 
+//        int a = 20;
+//        int b = 15;
+//         int sum  = a %b ;
+//         int c  = a -b ;
+//        System.out.println(  sum);
+//        System.out.print(c);
+
         int a = 10;
-        int b = 40;
-         int sum  = a +b ;
-         int c  = a -b ;
-        System.out.println(  sum);
-        System.out.print(c);
+        int b = 20;
+
+        System.out.println(a==b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a != b);
 
     }
 }
