1452aa0e537aa47e1c93d55b507a29845bdc94a5
unknown <gdutt2304@gmail.com>
2024-09-07 17:29:58 +0530

    Update doubt

--- class3.java
+++ class3.java
@@ -5,24 +5,52 @@ import java.util.Scanner;
 public class class3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        String a = "hello";
-//
+ //        String a = "hello";
 //        System.out.println( a + " world");
-        System.out.println("Enter your value");
+//        System.out.println("Enter your value");
+//        int a = sc.nextInt();
+//        if( a >= 60){
+//            System.out.println("first");
+//        }
+//        else if( a >= 40){
+//            System.out.println("second");
+//        }
+//        else if( a >= 30){
+//            System.out.println("third");
+//        }
+//        else{
+//            System.out.println("error");
+//        }
+
+
+        System.out.println("Enter your value : ");
         int a = sc.nextInt();
-        if( a >= 60){
-            System.out.println("first");
+        if(a==1){
+            System.out.println("Mon");
         }
-        else if( a >= 40){
-            System.out.println("second");
+        else if(a==2) {
+            System.out.println("Tue");
         }
-        else if( a >= 30){
-            System.out.println("third");
+        else if(a==3) {
+            System.out.println("Wed");
+        }
+        else if(a==4) {
+            System.out.println("Thurs");
+        }
+        else if(a==5) {
+            System.out.println("Fri");
+        }
+        else if(a==6) {
+            System.out.println("Sat");
+        }
+        else if(a == 7){
+            System.out.println("Sun");
         }
         else{
-            System.out.println("error");
-
+            System.out.println("Invalid weekday");
         }
     }
+    }
 
-}
+
+//(con)?true:false;
---
+++ class4.java
@@ -0,0 +1,26 @@
+package CoreJava;
+
+public class class4 {
+    public static void main(String[] args) {
+
+
+        String output  = "out";
+
+//        System.out.println("Hello, World!");
+        int a = 5;
+        String name = "Mannu";
+        System.out.println(name);
+        name = "Nikhil";
+//        a = 40;
+        if(name ==  "Mannu"){
+            output = "in";
+        }
+        else{
+//            System.out.println("out");
+            output = "out";
+        }
+        System.out.println(output); // Output: 40
+
+
+    }
+}
