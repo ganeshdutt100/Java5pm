633e7410232f9a22d6d2bd3b7e8e8c98b501599d
unknown <gdutt2304@gmail.com>
2024-09-13 18:31:24 +0530

    Update while loop

---
+++ class6.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class class6 {
+    public static void main(String[] args) {
+        int i = 1;  // initialization
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
