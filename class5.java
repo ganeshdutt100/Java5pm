2f29e8a511b5c26702033646bd96f4d883370186
unknown <gdutt2304@gmail.com>
2024-09-13 17:37:34 +0530

    Update Class 5

---
+++ class5.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class class5 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        for(int i =  0; i <= a; i++ ){
+            System.out.println(i);
+        }
+    }
+}
