6e85b09747b4774c2c1e14e82ca909d7389587ce
unknown <gdutt2304@gmail.com>
2024-09-14 17:27:00 +0530

    while loop

---
+++ class7.java
@@ -0,0 +1,29 @@
+package CoreJava;
+
+
+import java.util.Scanner;
+
+public class class7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Loop");
+//        for(int i =1; i <=20; i++){
+//            System.out.print(i + ",");
+//        }
+        int a = 10;
+        while(true){
+            System.out.println(a);
+            a++;
+        }
+
+
+
+
+    }
+}
+
+//++i
+
+//5 *1 = 5
+//5 *2 = 10
