35212f41bb1c1af00cdaf8105969825aa6946bb1
unknown <gdutt2304@gmail.com>
2024-10-05 17:49:57 +0530

    methods

--- Class12.java
+++ Class12.java
@@ -3,12 +3,30 @@ package CoreJava;
 public class Class12 {
     public static void main(String[] args) {
         System.out.println(" hello world");
-        Dipak();
-        Dipak();
-        Kriti();
+//        Dipak();
+//        Dipak();
+//        Kriti();
+int a = 10;
+int b = 30;
+int sum  = a +b;
+System.out.println(sum);
+
+int a1 = 10;
+int b1 = 30;
+int sum1  = a1 +b1;
+System.out.println(sum1);
+
+int  sum2 =  Sum(20,60);
+        System.out.println(sum2);
+
+        System.out.println(Sum(40, 70));
 
     }
 //-------------------------------------------------------------
+    public  static int Sum(int a , int b){
+        return  a+b;
+    }
+
     public static void Dipak(){
         System.out.println("i am Dipak");
     }
