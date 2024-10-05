c6b7999b742a9aebecbc3c17d998ebc285008e05
unknown <gdutt2304@gmail.com>
2024-10-05 18:05:55 +0530

    Update methods

--- Class12.java
+++ Class12.java
@@ -6,23 +6,29 @@ public class Class12 {
 //        Dipak();
 //        Dipak();
 //        Kriti();
-int a = 10;
-int b = 30;
-int sum  = a +b;
-System.out.println(sum);
-
-int a1 = 10;
-int b1 = 30;
-int sum1  = a1 +b1;
-System.out.println(sum1);
-
-int  sum2 =  Sum(20,60);
-        System.out.println(sum2);
-
-        System.out.println(Sum(40, 70));
-
+//int a = 10;
+//int b = 30;
+//int sum  = a +b;
+//System.out.println(sum);
+//
+//int a1 = 10;
+//int b1 = 30;
+//int sum1  = a1 +b1;
+//System.out.println(sum1);
+//
+//int  sum2 =  Sum(20,60);
+//        System.out.println(sum2);
+//
+//        System.out.println(Sum(40, 70));
+      Table(15);
     }
 //-------------------------------------------------------------
+    public static void Table(int a){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(a + " * " + i+ " = "+ a*i);
+        }
+    }
+
     public  static int Sum(int a , int b){
         return  a+b;
     }
