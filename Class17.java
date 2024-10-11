1cc4e890f212097fa2a46c955ee310aab71baf08
unknown <gdutt2304@gmail.com>
2024-10-11 17:32:34 +0530

    Update coreJava

---
+++ Class17.java
@@ -0,0 +1,9 @@
+package CoreJava;
+
+public class Class17 {
+    public static void main(String[] args) {
+        class16 surya = new class16();
+        surya.m1();
+        surya.m3();
+    }
+}
---
+++ class16.java
@@ -0,0 +1,16 @@
+package CoreJava;
+
+public class class16 {
+    public static void main(String[] args) {
+
+    }
+    public  void m1(){
+        System.out.println("This is m1 method");
+    }   public  void m4(){
+        System.out.println("This is m4 method");
+    }   public  void m2(){
+        System.out.println("This is m2 method");
+    }   public  void m3(){
+        System.out.println("This is m3 method");
+    }
+}
